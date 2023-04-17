b6f65fe70a50beda81f2bae442cf7ad7dcb64684
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-04-17 17:28:22 +0300

    Data

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingApp.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
