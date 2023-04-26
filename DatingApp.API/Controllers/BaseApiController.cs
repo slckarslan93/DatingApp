ecdc03f66676b001ef25c0ae5621fc61c6ed7891
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-04-26 19:43:47 +0300

    BaseApiController

---
+++ DatingApp.API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
